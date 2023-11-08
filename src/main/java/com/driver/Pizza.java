7ec88428ff7b762ea5332046ca1df4eb53bb3236
Pradeep Kattula <pradeepkattula390@gmail.com>
2023-11-08 21:13:46 +0530

    changes1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,30 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int cheesePrice;
+
+    private  int toppingsPrice;
+
+    private int bagPrice;
+
+    private boolean cheeseAdded;
+    private boolean toppingsAdded;
+    private boolean bagTaken;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+
+        if(isVeg){
+            this.price=300;
+            this.toppingsPrice=70;
+        }
+        else{
+            this.price=400;
+            this.toppingsPrice=120;
+        }
+        this.cheesePrice=80;
+        this.bagPrice=20;
     }
 
     public int getPrice(){
@@ -17,18 +38,34 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(cheeseAdded==false){
+            price=price+cheesePrice;
+            cheeseAdded=true;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(toppingsAdded==false){
+            price=price+toppingsPrice;
+            toppingsAdded=true;
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(bagTaken==false){
+            price=price+bagPrice;
+            bagTaken=true;
+        }
+
     }
 
     public String getBill(){
         // your code goes here
+        this.bill=price+"";
         return this.bill;
     }
 }
